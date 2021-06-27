adb9f0770164338c961e50590cd0527d73c419fc
iiTONELOC <anthonytropeano@me.com>
2021-06-26 22:44:30 -0400

    connected links

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -34,34 +34,50 @@ export default function Projects() {
                                 <Card.Body
                                     className='d-flex flex-wrap justify-content-start'
                                 >
-                                    <Card.Title className='col-12'>{project.name}</Card.Title>
+                                    <div className='col-12 projectTitle mb-2 border-dark border-bottom '>{project.name}</div>
                                     <Card.Text className='col-12'>
                                         {project.tech.map((el, idx) => (
                                             <Badge
                                                 variant={el.variant}
                                                 key={el.variant + idx}
-                                                className={`${el.bg ? `${el.bg}  p-2 mb-1 text-white textShadow boxShadow` : ` text-white p-2 mb-1 textShadow boxShadow`}`}
+                                                className={`${el.bg ? `${el.bg}  p-2 m-1 text-white textShadow boxShadow` : ` text-white p-2 mb-1 m-1 textShadow boxShadow`}`}
                                             >
                                                 {el.icon ? el.icon : el.name}
                                             </Badge>
                                         ))}
                                     </Card.Text>
-                                    <div className='col-12 d-flex flex-wrap justify-content-center'>
+                                    <div className='col-12 d-flex flex-wrap justify-content-start mt-5'>
                                         <Accordion.Collapse
                                             eventKey={wrapper}
                                             ref={wrapper}
                                         >
-                                            <div className="mb-2 col-12 d-flex flex-wrap justify-content-center">
-                                                <Button variant="primary" size="sm" className='m-1'>
+                                            <div >
+                                                <Button
+                                                    variant="primary"
+                                                    size="sm"
+                                                    className='m-1'
+                                                    href={project.url}
+                                                    target='_blank'
+                                                    rel='noreopener'
+                                                >
                                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-display" viewBox="0 0 16 16">
                                                         <path d="M0 4s0-2 2-2h12s2 0 2 2v6s0 2-2 2h-4c0 .667.083 1.167.25 1.5H11a.5.5 0 0 1 0 1H5a.5.5 0 0 1 0-1h.75c.167-.333.25-.833.25-1.5H2s-2 0-2-2V4zm1.398-.855a.758.758 0 0 0-.254.302A1.46 1.46 0 0 0 1 4.01V10c0 .325.078.502.145.602.07.105.17.188.302.254a1.464 1.464 0 0 0 .538.143L2.01 11H14c.325 0 .502-.078.602-.145a.758.758 0 0 0 .254-.302 1.464 1.464 0 0 0 .143-.538L15 9.99V4c0-.325-.078-.502-.145-.602a.757.757 0 0 0-.302-.254A1.46 1.46 0 0 0 13.99 3H2c-.325 0-.502.078-.602.145z" />
                                                     </svg>
+                                                    {''} View Live
                                                 </Button>
-                                                <Button variant="secondary" size="sm" className='m-1'>
+                                                <Button
+                                                    variant="secondary"
+                                                    size="sm"
+                                                    className='m-1'
+                                                    href={project.repo}
+                                                    target='_blank'
+                                                    rel='noreopener'
+                                                >
                                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-code-square" viewBox="0 0 16 16">
                                                         <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z" />
                                                         <path d="M6.854 4.646a.5.5 0 0 1 0 .708L4.207 8l2.647 2.646a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 0 1 .708 0zm2.292 0a.5.5 0 0 0 0 .708L11.793 8l-2.647 2.646a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708 0z" />
                                                     </svg>
+                                                    {''} Repo
                                                 </Button>
                                             </div>
                                         </Accordion.Collapse>
