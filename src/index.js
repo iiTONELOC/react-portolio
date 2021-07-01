c5c2add9608a63aaf04c312df7bbc2248f96ffb8
iiTONELOC <anthonytropeano@me.com>
2021-07-01 13:24:53 -0400

    sw

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-
+import * as serviceWorker from './serviceWorker';
 ReactDOM.render(
   //   <React.StrictMode>
   //   <App />
@@ -16,3 +16,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 
+serviceWorker.register();
