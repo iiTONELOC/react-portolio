7a0342294bc0266d5ecdc049594c9245c7b0db2e
iiTONELOC <anthonytropeano@me.com>
2021-07-01 13:42:17 -0400

    service worker on create-react-app does not work

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+
 ReactDOM.render(
   //   <React.StrictMode>
   //   <App />
@@ -12,8 +12,3 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-
-serviceWorker.register();
