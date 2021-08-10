904218ca062a4702deff327e15f340cf0a914618
iiTONELOC <anthonytropeano@me.com>
2021-08-10 14:47:55 -0400

    T

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -50,7 +50,7 @@ export default function Contact() {
             setFormState(initialState);
             setTimeout(() => {
                 setConfirmation(null);
-                window.location.assign('/react-portfolio')
+                window.location.assign('/react-portolio')
             }, 4000);
 
         }
